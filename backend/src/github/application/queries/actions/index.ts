966dea570b81185dc288347e35428b0b30d22a23
Miguel Jiménez <migueljimenez@Miguel319.local>
2022-11-02 03:44:29 -0400

    feat: add fetch commits from repository query

--- backend/src/github/application/queries/actions/index.ts
+++ backend/src/github/application/queries/actions/index.ts
@@ -1,1 +1,2 @@
 export { FetchBranchesFromRepositoryQuery } from "./fetch-branches-from-repository.query";
+export { FetchCommitsFromRepositoryQuery } from "./fetch-commits-from-repository.query";
---
+++ backend/src/github/application/queries/handlers/index.ts
@@ -0,0 +1,2 @@
+export { FetchBranchesFromRepositoryQueryHandler } from "./fetch-branches-from-repository-query.handler";
+export { FetchCommitsFromRepositoryQueryHandler } from "./fetch-commits-from-repository-query.handler";
