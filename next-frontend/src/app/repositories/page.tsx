17505fd20abf59d39c2fb8b2bd02f4e6d29f2266
Miguel Jiménez <migueljimenez@Miguel319.local>
2022-11-03 14:33:22 -0400

    rename projects ui projects

---
+++ next-frontend/src/app/repositories/page.tsx
@@ -0,0 +1,18 @@
+import { GitHubRepositoryCard } from "../../components";
+import { GitHubRequest } from "../../core/api/requests";
+
+export default async () => {
+  const repos = await GitHubRequest.getRepositoriesFromUser("Miguel319");
+
+  return (
+    <>
+      <title>GitHub Repositories</title>
+
+      <div className="grid p-3 gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {repos?.map((repo) => (
+          <GitHubRepositoryCard repository={repo} />
+        ))}
+      </div>
+    </>
+  );
+};
